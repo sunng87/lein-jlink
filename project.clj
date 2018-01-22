fd11f1530263e2c16e15e31cc356099e7d66fe48
Ning Sun <sunng@about.me>
2018-01-22 22:09:45 +0800

    (chore) added clojars
    Signed-off-by: Ning Sun <sunng@about.me>

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/sunng87/lein-jlink"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :deploy-repositories {"releases" :clojars})
