0a9d96609ecb86c177ec1a80de3622307b973173
Ning Sun <sunng@protonmail.com>
2019-01-23 22:29:58 +0800

    Version 0.2.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-jlink "0.2.1"
+(defproject lein-jlink "0.2.2-SNAPSHOT"
   :description "Package your Leiningen project as a standalone and stripped-down JVM using Java's jlink. Great for distribution and Docker images."
   :url "https://github.com/sunng87/lein-jlink"
   :license {:name "Eclipse Public License"
