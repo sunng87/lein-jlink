7054fa5b442cb2ab9707b42a9ea31b7be82b159a
Christopher Miles <twitch@nervestaple.com>
2020-04-18 21:56:00 -0400

    Moved 'jlink' out of the 'target' directory

--- src/leiningen/jlink.clj
+++ src/leiningen/jlink.clj
@@ -2,11 +2,14 @@
   (:refer-clojure :exclude [test])
   (:require [leiningen.core.eval :as eval]
             [leiningen.core.main :as l]
+            [leiningen.clean :as clean]
             [leiningen.run :as run]
             [leiningen.test :as test]
             [leiningen.uberjar :as uberjar]
             [leiningen.jar :as jar]
             [leiningen.help :as h]
+            [leiningen.core.project :as p]
+            [robert.hooke :as hooke]
             [clojure.java.io :as io]
             [clojure.string :as s])
   (:import [java.io File]))
@@ -22,7 +25,10 @@
   (io/delete-file f true))
 
 (defn out [project]
-  (str (:target-path project) "/jlink"))
+  (let [parent (.getParent (File. (:target-path project)))]
+       (str parent
+            (File/separator)
+            "jlink")))
 
 (defn- print-help []
   (h/help nil "jlink"))
@@ -57,54 +63,64 @@
       (spit config-cache (pr-str jlink-modules))
       (l/info "Created" jlink-path))))
 
+(defn middleware [project]
+  (let [java-home (System/getenv "JAVA_HOME")
+        jlink-bin-path (s/join (File/separator) [java-home "bin" "jlink"])
+        jlink-modules-path (str "\""
+                                (s/join (File/pathSeparator)
+                                        (concat (:jlink-module-paths project)
+                                                [(str java-home "/jmods")]))
+                                "\"")
+        jlink-modules (s/join "," (concat (:jlink-modules project) ["java.base"]))
+        cached-modules (try
+                         (read-string (slurp config-cache))
+                         (catch Exception _))
+        jlink-path (out project)]
+    (merge project
+           {:java-cmd (s/join (File/separator) [jlink-path "bin" "java"])}
+           {:javac-options ["--module-path" jlink-modules-path "--add-modules" jlink-modules]})))
+
+(defn compile-hook [task & args]
+ (apply task args))
+
 (defn clean
   "Cleanup jlink environment"
   [project]
-  (delete-directory (io/file (out project))))
+  (delete-directory (io/file (out project)))
+  (clean/clean project))
 
 (defn java-exec [project] (str (out project) "/bin/java"))
 
 (defn run
   "lein run using jlink java"
   [project args]
-  (jlink project "init")
+  (init project)
   (let [project (assoc project :java-cmd (java-exec project))]
+    (l/info (str (:java-cmd project)))
     (apply run/run project args)))
 
 (defn test
   "lein test using jlink java"
   [project args]
-  (jlink project "init")
+  (init project)
   (let [project (assoc project :java-cmd (java-exec project))]
     (apply test/test project args)))
 
 (defn assemble
   "Assemble a portable java environment"
   [project]
+  (init project)
   (let [jar-path (uberjar/uberjar project)
         jlink-path (out project)
         executable (str jlink-path "/bin/" (:name project))]
-    (jlink project "init")
-    (eval/sh "cp" jar-path (str jlink-path "/" (:name project) ".jar"))
-    (l/info "Copied uberjar into" jlink-path)
-    (spit executable
-          (format (slurp (io/resource "exec.sh")) (:name project)))
-    (eval/sh "chmod" "a+x" executable)
-    (l/info "Created executable" executable)))
+    (io/copy (io/file jar-path) (io/file (str jlink-path "/" (:name project) ".jar")))
+    (l/info "Copied uberjar into" jlink-path)))
 
 (defn package
   "Create a tarball of the portable environment"
   [project]
   (assemble project)
-  (let [tarball-name (str (:target-path project)
-                          "/"
-                          (:name project)
-                          "-"
-                          (:version project)
-                          "-jlink"
-                          ".tar.gz")]
-    (eval/sh "tar" "czf" tarball-name "-C" (out project) ".")
-    (l/info "Created tarball" tarball-name)))
+  (l/info "Someday we'll call jpackage and really do something! :-D"))
 
 
 (defn ^{:help-arglists '[[project sub-command]]
