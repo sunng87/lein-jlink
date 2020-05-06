be15ce29513408960363067a6b42f00ab4dd82be
Ning Sun <sunng@protonmail.com>
2020-05-06 17:30:39 +0800

    Version 0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-jlink "0.3.0-SNAPSHOT"
+(defproject lein-jlink "0.3.0"
   :description "Package your Leiningen project as a standalone and stripped-down JVM using Java's jlink. Great for distribution and Docker images."
   :url "https://github.com/sunng87/lein-jlink"
   :dependencies [[org.apache.commons/commons-compress "1.20"]]
